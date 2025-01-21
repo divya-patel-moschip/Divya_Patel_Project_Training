847b64058f7710e75cdd3f0f0536bb9ba7e7995d
divya patel <divya.patel@moschip.com>
2025-01-21 10:52:48 +0530

    different try

--- tree.py
+++ tree.py
@@ -1,38 +1,3 @@
-# import xml.etree.ElementTree as ET
-# from xml.dom import minidom
-#
-# root = ET.Element("testsuites")
-#
-# LOG_ARRAY = []
-#
-# with open('mylog.log', 'r') as log_file:
-#     for index, line in enumerate(log_file):
-#         if index <=4:
-#             LOG_ARRAY.append(f"{index}: {line.strip()}")
-#
-#
-#
-# for i in range(5):
-#     ele1 = ET.Element("testsuite")
-#     ele2 = ET.Element('testcase')
-#     ele3 = ET.Element("build-info")
-#     ele2.text = f"Test-{i}"
-#     ele3.text = LOG_ARRAY[i]
-#     ele1.append(ele2)
-#     ele1.append(ele3)
-#     root.append(ele1)
-#
-# rough_string = ET.tostring(root, 'utf-8')
-#
-# parsed = minidom.parseString(rough_string)
-# pretty_xml = parsed.toprettyxml(indent="    ")
-#
-# with open('report.xml', 'w') as f:
-#     f.write(pretty_xml)
-#
-# print("Formatted XML written to 'report.xml'")
-
-
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
@@ -42,10 +7,10 @@ LOG_ARRAY = []
 
 with open('mylog.log', 'r') as log_file:
     for index, line in enumerate(log_file):
-        if index <=3:
+        if index <=5:
             LOG_ARRAY.append(f"{index}: {line.strip()}")
 
-for i in range(4):
+for i in range(6):
     testsuite = ET.Element("testsuite", {
         "name": f"Suite-{i}",
         "tests": "0",
@@ -66,6 +31,11 @@ for i in range(4):
         })
         failure.text = f"Details about the failure in Test-{i}."
         testcase.append(failure)
+    elif i >= 3:
+        skipped = ET.Element("skipped", {
+            "message": f"Test-{i} was skipped by user"
+        })
+        testcase.append(skipped)
 
     testsuite.append(testcase)
     root.append(testsuite)
